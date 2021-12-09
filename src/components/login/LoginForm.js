70aaaa9d6bcb89a779e3efb1c96dfcb1bc933b5d
Cantaloop Comunicação <https://github.com/evandromottaz/site-animais-fantastico.git>
2021-12-09 07:48:14 -0300

    useEffect para local storage criado

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -3,14 +3,23 @@ import { Link } from 'react-router-dom';
 import useForm from '../../hooks/useForm';
 import Button from '../form/Button';
 import Input from '../form/Input';
-import { TOKEN_POST } from '../../api';
+import { TOKEN_POST, USER_GET } from '../../api';
 
 const LoginForm = () => {
   const username = useForm(); //value, setValue, onChange
   const password = useForm();
 
+  React.useEffect(() => {
+    // verifica se o token já está no local storage
+    const token = window.localStorage.getItem('token');
+    if (token) getUser(token);
+  }, []);
+
   async function getUser(token) {
-    const response = fetch();
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
   }
 
   async function handleSubmit(event) {
