85829b92392d259b373a7287e3b82ca3a648ef10
Cantaloop Comunicação <https://github.com/evandromottaz/site-animais-fantastico.git>
2022-01-17 14:55:30 -0300

    password validation added on hook useForm

--- src/components/login/LoginCreate.js
+++ src/components/login/LoginCreate.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import useForm from '../../hooks/useForm';
+import Button from '../form/Button';
+import Input from '../form/Input';
+import { Section } from '../styles/Form.styled';
+import { Title } from '../styles/Text.styled';
 
 const LoginCreate = () => {
-  return <div>Login Criar</div>;
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm('password');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <Section>
+      <Title>Cadastre-se</Title>
+      <form onSubsmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" />
+        <Button>Cadastrar</Button>
+      </form>
+    </Section>
+  );
 };
 
 export default LoginCreate;
