5385a9e3292d0f8bfe515c6a75959b256dc57862
evandromottaz <evandro.motta@cantacom.com.br>
2022-04-21 09:17:26 -0300

    lost password

--- src/components/login/LoginPasswordLost.js
+++ src/components/login/LoginPasswordLost.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { PASSWORD_LOST } from '../../api';
+import useFetch from '../../hooks/useFetch';
+import useForm from '../../hooks/useForm';
+import Button from '../form/Button';
+import Input from '../form/Input';
+import Error from '../helper/Error';
+import { Title } from '../styles/Text.styled';
 
 const LoginPasswordLost = () => {
-  return <div></div>;
+  const login = useForm();
+  const { data, loading, error, request } = useFetch();
+
+  async function handleSubmit(ev) {
+    ev.preventDefault();
+    const { url, options } = PASSWORD_LOST({
+      login: login.value,
+      url: window.location.href.replace('perdeu', 'resetar'),
+    });
+    const { json } = await request(url, options);
+  }
+  return (
+    <section>
+      <Title>Perdeu a senha?</Title>
+      {data ? (
+        <p>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email ou usuário" type="text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando</Button>
+          ) : (
+            <Button>Enviar e-mail</Button>
+          )}
+
+          <Error error={error} />
+        </form>
+      )}
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
