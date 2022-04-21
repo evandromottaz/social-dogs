b3c6cee5ff48e2d3a5a40f709c7cd51429bfc4a5
evandromottaz <evandro.motta@cantacom.com.br>
2022-04-21 09:37:38 -0300

    reset password

--- src/components/login/LoginPasswordReset.js
+++ src/components/login/LoginPasswordReset.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import useFetch from '../../hooks/useFetch';
+import useForm from '../../hooks/useForm';
+import Button from '../form/Button';
+import Input from '../form/Input';
+import { PASSWORD_RESET } from '../../api';
+import { Title } from '../styles/Text.styled';
+import Error from '../helper/Error';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPasswordReset = () => {
-  return <div></div>;
+  const [login, setLogin] = React.useState('');
+  const [key, setKey] = React.useState('');
+  const password = useForm();
+  const { error, loading, request } = useFetch();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    // url vinda da url de reset enviado no email
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get('key');
+    const login = params.get('login');
+
+    if (key) setKey(key);
+    if (login) setLogin(login);
+  }, []);
+
+  async function handleSubmit(ev) {
+    ev.preventDefault();
+    if (password.validate()) {
+      const { url, options } = PASSWORD_RESET({
+        login,
+        key,
+        password: password.value,
+      });
+      const { response } = await request(url, options);
+      if (response.ok) navigate('/login');
+    }
+  }
+  return (
+    <div>
+      <Title>Resete a senha</Title>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Nova senha"
+          type="password"
+          name="password"
+          {...password}
+        />
+        {loading ? (
+          <Button disabled>Resetando...</Button>
+        ) : (
+          <Button>Resetar</Button>
+        )}
+      </form>
+      <Error error={error} />
+    </div>
+  );
 };
 
 export default LoginPasswordReset;
